4f6f5fcce8826b628580fc9e2b7ec76c776b6ede
D-MAN1 <vitalij.olgovich@mail.ru>
2023-01-28 18:35:45 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'akopov05@yandex.ru'),
+        'address' => env('MAIL_FROM_ADDRESS', '..@yandex.ru'),
         'name' => env('MAIL_FROM_NAME', 'LazerRez'),
     ],
 
